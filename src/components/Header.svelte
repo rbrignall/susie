ff7dd650043b95c9d518a4972eb877b228367f49
rbrignall <rbrignall@gmail.com>
2022-07-12 22:29:11 +0100

    Hotfix: 2.0.1
    Fix issue with game not refreshing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -38,7 +38,7 @@
 				    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10zm4.82-4.924a7 7 0 1 0-1.852 1.266l-.975-1.755A5 5 0 1 1 17 12h-3l2.82 5.076z"
 				/>
 			</GameIcon>            
-        {:else if guesses > 9 && gameStatus === "IN_PROGRESS"}
+        {:else if (guesses > 9 && gameStatus === "IN_PROGRESS")}
         <div class="giveup" on:click={e => dispatch("giveup")}>Give up?</div>
         {:else}
 			<GameIcon onClick={e => dispatch("toggleMode")} Tooltip="Toggle unlimited mode">
